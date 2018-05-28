a48ee6a1e482c7e3a970751c978b8ecd7eb7e8cb
Anton Alexandrovich <33841060+Gorachevskiy@users.noreply.github.com>
2018-05-28 20:55:09 +0200

    Add files via upload

---
+++ src/main/java/com/example/anton/proyecto010/AcercaDe.java
@@ -0,0 +1,19 @@
+package com.example.anton.proyecto010;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AcercaDe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de);
+    }
+    public void salir(View v) {
+        finish();
+    }
+}
